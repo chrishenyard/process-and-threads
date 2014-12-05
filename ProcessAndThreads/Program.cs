458e1472fea9d42c7abe79f29bf3c848147fea66
chrishenyard <chenyard@gmail.com>
2014-12-05 09:35:25 -0700

    Initial commit

---
+++ ProcessAndThreads/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProcessAndThreads {
+    class Program {
+        static void Main(string[] args) {
+            var tasks = new List<Task>();
+            Process process = Process.GetCurrentProcess();
+
+            for (int i = 0; i < 2; i++) {
+                var threadCount = i + 1;
+                tasks.Add(Task.Factory.StartNew(() => {
+                    Console.WriteLine(string.Format("Thread number {0} in ProcessID {1}", threadCount, process.Id));
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
